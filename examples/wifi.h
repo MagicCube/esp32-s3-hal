ebe6a1e6b4df09d4bcd171c2034dd231aaf1d617
Henry Li <henry1943@163.com>
2025-01-24 22:42:37 +0800

    fix: simplify

--- examples/wifi.h
+++ examples/wifi.h
@@ -1,19 +1,9 @@
 #pragma once
 
-#include <time.h>
-
-bool wifi_connected = false;
-
 void wifi_setup() {
   WiFiConnector.addKnownSSID("Henry's Living Room 2.4GHz", "13913954971");
   WiFiConnector.addKnownSSID("Henry's iPhone 14 Pro", "13913954971");
   WiFiConnector.begin();
 }
 
-void wifi_loop() {
-  WiFiConnector.update();
-  if (!wifi_connected &&
-      WiFiConnector.state() == WIFI_CONNECTOR_STATE_CONNECTED) {
-    wifi_connected = true;
-  }
-}
+void wifi_loop() { WiFiConnector.update(); }
