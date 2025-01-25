8eb38e4649a6923a0b84186a7be0a81450722d39
Henry Li <henry1943@163.com>
2025-01-25 11:43:52 +0800

    feat: add app icons

--- examples/demo/ui/scenes/home_scene.cpp
+++ examples/demo/ui/scenes/home_scene.cpp
@@ -6,20 +6,40 @@ HomeScene* HomeScene::_instance = nullptr;
 
 LV_IMG_DECLARE(img_home_scene_bg);
 
+AppInfo APP_INFO_LIST[4] = {
+    {"select_agent_scene", "通话", "\uf095", rgb(0x31C959)},
+    {"camera_scene", "相机", "\uf030", rgb(0x36BC9B)},
+    {"music_scene", "音乐", "\uf001", rgb(0xE64D3D)},
+    {"settings_scene", "设置", "\uf013", rgb(0xA6AFB6)},
+};
+
 void HomeScene::onInit() {
   MXScene::onInit();
 
   root()->bg_black();
 
-  button = root()
-               ->add_button("\uf095")
-               ->size(100, 100)
-               ->center_x()
-               ->y(94)
-               ->rounded(26)
-               ->bg(rgb(0x31C959))
-               ->icon_font(48)
-               ->on_click([this](MXEvent* e) {
-                 SelectAgentScene::instance()->show();
-               });
+  appIconsContainer = root()
+                          ->add_object()
+                          ->flex(LV_FLEX_FLOW_ROW_WRAP)
+                          ->gap(32, 12)
+                          ->p_x(16)
+                          ->w_full()
+                          ->y(40)
+                          ->scrollable(true)
+                          ->scroll_bar_hidden();
+  uint16_t count = sizeof(APP_INFO_LIST) / sizeof(APP_INFO_LIST[0]);
+  for (uint16_t i = 0; i < count; i++) {
+    AppInfo& appInfo = APP_INFO_LIST[i];
+    MXObject* appIconContainer =
+        appIconsContainer->add_object()
+            ->flex_col(LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER,
+                       LV_FLEX_ALIGN_CENTER)
+            ->gap(0, 4);
+    MXObject* appIcon = appIconContainer->add_button(appInfo.icon)
+                            ->size(88)
+                            ->rounded(20)
+                            ->bg(appInfo.color)
+                            ->icon_font(48);
+    MXObject* appName = appIconContainer->add_label(appInfo.name);
+  }
 }
--- examples/demo/ui/scenes/home_scene.h
+++ examples/demo/ui/scenes/home_scene.h
@@ -1,13 +1,22 @@
 #pragma once
 
-#include <button.h>
+#include <lvgl.h>
 #include <mx.h>
 
 #include "../views/agent_card_roller.h"
 
+struct AppInfo {
+  const char* id = "";
+  const char* name = "";
+  const char* icon = "";
+  const lv_color_t color = lv_color_black();
+};
+
+extern AppInfo APP_INFO_LIST[4];
+
 class HomeScene : public MXScene {
  public:
-  ~HomeScene() { delete button; }
+  ~HomeScene() { delete appIconsContainer; }
 
   inline static HomeScene* instance() {
     if (_instance == nullptr) {
@@ -18,7 +27,7 @@ class HomeScene : public MXScene {
   }
 
  protected:
-  MXObject* button;
+  MXObject* appIconsContainer;
   void onInit() override;
 
  private:
