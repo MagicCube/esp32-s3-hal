5dee3b3828b70fa027455cb0e717fccb95d6c682
Henry Li <henry1943@163.com>
2025-01-25 10:44:24 +0800

    feat: add icons

--- scripts/fonts.js
+++ scripts/fonts.js
@@ -2,4 +2,13 @@ const { subsetFont, subsetFontAwesome } = require('./subset-font');
 const { convertToCFile } = require('./convert-to-c-file');
 
 subsetFont('PingFang Medium');
-subsetFontAwesome(['f095', 'f130']);
+subsetFontAwesome([
+  'e2ca', // wand-magic-sparkles
+  'f001', // music
+  'f013', // gear
+  'f030', // camera
+  'f095', // phone
+  'f130', // microphone
+  'f1eb', // wifi
+  'f4ff', // user-pen
+]);
