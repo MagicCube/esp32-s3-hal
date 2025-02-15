4a684393b5134b52aef5b5fe0d4e6878524ff804
Henry Li <henry1943@163.com>
2025-02-15 16:37:15 +0800

    fix: add wifi_connector

--- examples/wifi.h
+++ examples/wifi.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <wifi_connector.h>
+
 void wifi_setup() {
   WiFiConnector.addKnownSSID("Henry's Living Room 2.4GHz", "13913954971");
   WiFiConnector.addKnownSSID("Henry's iPhone 14 Pro", "13913954971");
